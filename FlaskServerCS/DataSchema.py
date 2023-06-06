2852a6732db4d0d9a1e216fb53c7059c65f179ca
Aman Sood <soodaman92@gmail.com>
2023-06-06 11:55:49 +0530

    Delete FlaskServerCS directory

--- FlaskServerCS/DataSchema.py
+++
@@ -1,9 +0,0 @@
-
-def makeSchema(recieved_array):
-    
-    key_names={"screen_size", "4g", "5g", "rear_camera_mp", "front_camera_mp",
-                    "internal_memory", "ram", "battery", "weight", "release_year", "days_used","normalized_new_price"}
-
-    dict_schema = {key: value for key, value in zip(key_names, recieved_array)}
-    
-    return dict_schema
--- FlaskServerCS/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    MongoP(received_array)
